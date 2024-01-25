f3fcc2d7c1903a770f55301e46864a40db6585db
sungrae <sungrae@synopsys.com>
2024-01-25 18:08:49 +0900

    add db.json

--- 2024-01-shopping-mall/src/app/app.component.ts
+++ 2024-01-shopping-mall/src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shopping-mall';
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get('/assets/db.json').pipe(
+      tap(res => console.log({res}))
+    ).subscribe();
+  }
 }
